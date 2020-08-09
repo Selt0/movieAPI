11989eaf0939aaf3910bf1fa962b827e2d9d5ca5
Selt0 <chewedgum12@gmail.com>
2020-08-09 10:08:19 -0500

    Complete query

--- app.js
+++ app.js
@@ -2,28 +2,35 @@
 import express from 'express';
 import { OMDb_key } from './keys.js';
 import axios from 'axios';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-let title = 'Frozen';
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
+let title = 'the incredibles';
 app.get('/', (req, res) => {
-  res.render('index.html');
+  res.render('index');
 });
 
 app.get('/results', (req, res) => {
-  res.send(title);
+  axios
+    .get(`http://www.omdbapi.com/?apikey=${OMDb_key}&s=${title}`)
+    .then((response) => {
+      res.render('results', { movies: response.data['Search'] });
+    })
+    .catch((error) => {
+      res.send('There was an unexpected error :(');
+      console.log(error);
+    });
 });
 
-axios
-  .get(`http://www.omdbapi.com/?apikey=${OMDb_key}&t=${title}`)
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.log("didn't work");
-  });
+app.post('/search', (req, res) => {
+  title = req.body.title;
+  res.redirect('/results');
+});
 
 app.listen(port, () => {
   console.log('Server is live!');
